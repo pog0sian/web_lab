e5baf3d28382089b9f3fd76d6b68f9459a004477
pog0sian <126772806+pog0sian@users.noreply.github.com>
2025-10-06 00:56:24 +0700

    Implement smooth scrolling for anchor links
    Added smooth scrolling for anchor links.

--- js/script.js
+++ js/script.js
@@ -81,5 +81,14 @@ $(function() {
     });
 });
 
-
+// Предотвращение перехода по якорю и плавный скролл
+$('a[href^="#"]').on('click', function(e) {
+  e.preventDefault();
+  const target = $($(this).attr('href'));
+  if (target.length) {
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 600);
+  }
+});
 
